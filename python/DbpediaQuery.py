e52ac62e26cb2c95e0bc0c311e732b96a2e65ed7
husonchen <1092441751@qq.com>
2016-10-27 11:35:09 +0800

    改成交叉对比每次的broader

--- python/DbpediaQuery.py
+++ python/DbpediaQuery.py
@@ -23,12 +23,11 @@ class DbpediaQuery :
     def getConceptsOfThing(self,thing) :
         thing = thing.lower()
         cur = self.con.cursor()
-        cur.execute("SELECT * FROM subject WHERE resource='%s'" %thing)
+        cur.execute("SELECT concept FROM subject WHERE resource='%s'" %thing)
         rows = cur.fetchall()
         concepts = []
-        for row in rows:
-            concepts.append(row[1])
-
+        for row in rows :
+            concepts.append(row[0].encode('utf-8'))
         return concepts
 
     '''
@@ -40,12 +39,17 @@ class DbpediaQuery :
         # if tag is not a concept, first find the concept
         concepts = self.getConceptsOfThing(tag)
         if len(concepts) != 0 :
-            tag = concepts[0] #need further discuss
-            #cost 1 depth to find the concept
+            tag = concepts
             depth -= 1
-        return self.searcher.searchBroaderCategory(tag, depth)
+        return list(self.searchBroaderCategory(tag, depth))
 
+    def searchBroaderCategory(self,concepts,depth):
+        # if depth <= 0 :
+        #     return concepts
 
+        for i in range(depth):
+            concepts = self.findBroaderCategories(concepts)
+        return concepts
 
     '''
     find the smallest distance between two things or concept
@@ -66,25 +70,72 @@ class DbpediaQuery :
         else :
             return False
 
-        shortestPath = []
-        shortlength = 65535
-        for i in concepts1:
-            path = self.searcher.shortestPathBetweenCategores(i,concepts2[0])
-            if len(path) < shortlength:
-                shortlength = len(path)
-                shortestPath = path
-                shortestStart = concepts1[i]
+        pathLength = self.deepFirstSearch(concepts1,concepts2)
 
-        for i in concepts2:
-            path = self.searcher.shortestPathBetweenCategores(shortestStart,i)
-            if len(path) < shortlength:
-                shortlength = len(path)
-                shortestPath = path
-                shortestStart = concepts2[i]
-
-        if len(shortestPath) == 0:
+        if pathLength == False:
             return False
-        return  head + shortestPath + tail
+        return  pathLength + 2
+
+    def deepFirstSearch(self,concepts1,concepts2):
+        startSetList = [set(concepts1)] # start set,every element of this list is a set
+        endSetList = [set(concepts2)]
+
+        met = startSetList[0].intersection(endSetList[0])
+        if len(met) != 0 :
+            return 1
+        # if(met)
+        maxDepth = 6
+        shortlength = 65535
+        isFind = False
+        for i in range(0,maxDepth):
+            # find the entry of this depth
+            broaderCategories = self.findBroaderCategories(startSetList[i])
+            #compare to every categories in endSetList
+            for j in range(len(endSetList)):
+                inter = endSetList[j].intersection(broaderCategories)
+                if len(inter) != 0 :
+                    # arealdy find the path
+                    shortlength = i + j + 2
+                    isFind = True
+                    break
+            if isFind :
+                break
+            # not find the path, store it
+            startSetList.append(broaderCategories)
+
+            # next extend end concept
+            broaderCategories = self.findBroaderCategories(endSetList[i])
+            # compare to every categories in startSetList
+            for j in range(len(endSetList)):
+                inter = startSetList[j].intersection(broaderCategories)
+                if len(inter) != 0:
+                    # arealdy find the path
+                    shortlength = i + j + 2
+                    isFind = True
+                    break
+            if isFind :
+                break
+            endSetList.append(broaderCategories)
+
+        if isFind :
+            return shortlength
+        else:
+            return False
+
+
+    def findBroaderCategories(self,concepts):
+        concepts = tuple(concepts)
+        cur = self.con.cursor()
+        sql = "SELECT broader FROM category WHERE concept in %s" % str(concepts)
+        print sql
+        cur.execute(sql)
+        rows = cur.fetchall()
+        concepts = set()
+        for row in rows:
+            concepts.add(row[0].encode('utf-8'))
+
+        return concepts
+
 
 if __name__ == '__main__':
     query = DbpediaQuery()
@@ -94,6 +145,6 @@ if __name__ == '__main__':
     print "SuperClass 1 of color : " + str(query.getSuperClass("color"))
     import time
     start = time.time() * 1000
-    print "SmallestDistance between water and lake " + str(query.getSmallestDistance("water","lake"))
+    print "SmallestDistance between water and lake " + str(query.getSmallestDistance("blue","airline"))
     end = time.time() * 1000
     print "cost time "+ str(end - start)
